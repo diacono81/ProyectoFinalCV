184d452d9258a43c41fb70835651e4467ac0bc9d
Oscar Pinedo <126519272+diacono81@users.noreply.github.com>
2023-03-16 10:07:26 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,10 @@
+document.getElementById('boton').onclick = function newDoc()
+{  
+
+window.location.assign("https://www.linkedin.com") 
+  
+console.log("Capturamos el evento clic");  
+
+    
+    
+}
